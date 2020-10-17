e21254bad3383425b2180717a3573f0dc0903113
vinayaachar <vinayaachar@gmail.com>
2020-10-17 11:14:24 -0700

    Complete through defaults

--- src/underbar.js
+++ src/underbar.js
@@ -201,13 +201,24 @@
 
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
-    // TIP: Try re-using reduce() here.
+    // TIP: Try re-using reduce() here
+    if (collection.length === 0) {return true;}
+    if(iterator === undefined) {iterator = _.identity}
+    return !!_.reduce(collection, function(matchTest, item){
+      return matchTest && iterator(item);
+    }, true);
+
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    if (collection.length === 0) {return false;}
+    if(iterator === undefined) {iterator = _.identity}
+    return !!_.reduce(collection, function(matchTest, item){
+      return matchTest || iterator(item);
+    }, false);
   };
 
 
@@ -230,11 +241,25 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    _.each(arguments, function(sample){
+      _.each(sample, function(value, key){
+        obj[key] = value;
+      })
+    })
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    _.each(arguments, function(sample){
+      _.each(sample, function(value, key){
+        if(obj[key] === undefined){
+          obj[key] = value;
+        }
+      })
+    })
+    return obj;
   };
 
 
